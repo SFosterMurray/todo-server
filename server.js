5ee315f7484cfa5b17c83a49b202cca7bc990cd2
DREAMS\SFosterMurray <SFosterMurray@comcast.net>
2016-09-14 21:17:15 -0400

    just a small git test

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var bcrypt = require('bcrypt');
 var middleware = require('./middleware.js')(db);
 
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8000;
 var todos = [];
 var todoNextId = 1;
 
